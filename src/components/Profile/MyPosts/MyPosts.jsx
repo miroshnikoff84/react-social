d660528338d94ac0f897f0d19a38a2857a650a9b
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-08-24 02:51:21 +0300

    add object in my posts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+
+  let postData = [
+    {id: 1, message: 'Hi', likesCount: 12},
+    {id: 2, message: '24 урок', likesCount: 11}
+  ]
+
   return <div className={s.postsBlock}>
     My posts
       <div>
@@ -14,8 +20,8 @@ const MyPosts = (props) => {
       </div>
     </div>
     <div className={s.posts}>
-    <Post message='Hi, are you?' likesCount='10'/>
-    <Post message="It's my first post" likesCount='24' />
+    <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+    <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
         </div>
   </div>
 }
